f071d1798b5e29492ba37d34522837af2788c8a2
souleymanedansokho018 <slmdsk77@gmail.com>
2025-04-07 11:30:57 +0200

    Create exo3.php

---
+++ exo3.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+$b = 5;
+$somme = $a + $b;
+$diff = $a - $b;
+$produit = $a * $b;
+$division = $a / $b;
+echo "Somme : " . $somme . "<br>";
+echo "Différence : " . $diff . "<br>";
+echo "Produit : " . $produit . "<br>";
+echo "Division : " . $division . "<br>";
+?>
