4037d856e5b44673d232de181f5729e3a8812d89
souleymanedansokho018 <slmdsk77@gmail.com>
2025-04-10 15:21:45 +0200

    Update exo9.php

--- exo9.php
+++ exo9.php
@@ -1,8 +1,5 @@
 <?php
 $fruits = array('pomme', 'banane', 'orange');
-
-for ($i = 0; $i < count($fruits); $i++) {
-  echo $fruits[$i] . "\n";
-}foreach ($fruits as $fruit) {
+foreach ($fruits as $fruit) {
     echo $fruit . "\n";
   }
